1e65ea1ee1d2a8426c0e2d3c95478bce77326cc2
Massimo Melina <a@rejetto.com>
2024-10-25 19:45:49 +0200

    nicer config dialog

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,11 +1,11 @@
-exports.version = 1.01
+exports.version = 1.02
 exports.description = "Expand file-show to support any file format supported by the browser"
 exports.apiRequired = 8.22
 exports.frontend_js = "main.js"
 exports.repo = "rejetto/show-any"
 
 exports.configDialog = {
-    sx: { maxWidth: '15em' },
+    sx: { maxWidth: '16em' },
 }
 
 exports.config = {
