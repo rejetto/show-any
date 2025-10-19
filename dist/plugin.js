fecd5e6dd29d227f116e0c329b5bc098a8d5a3fa
Massimo Melina <a@rejetto.com>
2025-10-19 14:42:06 +0200

    compatibility with firefox 52

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,9 +1,11 @@
-exports.version = 1.02
+exports.version = 1.03
 exports.description = "Expand file-show to support any file format supported by the browser"
 exports.apiRequired = 8.22
 exports.frontend_js = "main.js"
 exports.repo = "rejetto/show-any"
-
+exports.changelog = [
+    { "version": 1.03, "message": "compatibility with firefox 52" },
+]
 exports.configDialog = {
     sx: { maxWidth: '16em' },
 }
--- dist/public/main.js
+++ dist/public/main.js
@@ -1,13 +1,12 @@
 'use strict';{
     const exts = HFS.getPluginConfig().extensions.map(x => x.ext)
 
-    HFS.onEvent('fileShow', ({ entry: { ext } }) => 
+    HFS.onEvent('fileShow', ({ entry: { ext } }) =>
         exts.includes(ext) && ShowFrame)
 
     function ShowFrame(props) {
-        return HFS.h('iframe', { 
-            ...props,
-            style: { ...props.style, width: '100%', height: '100%', background: 'var(--bg)' } 
-        })
+        return HFS.h('iframe', _.merge(props, {
+            style: { width: '100%', height: '100%', background: 'var(--bg)' }
+        }))
     }
 }
